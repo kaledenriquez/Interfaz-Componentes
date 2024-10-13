51387c1977d95ef4193be65c53541bae1e605591
Kaled Enríquez <kaledtrejo@hotmail.com>
2024-10-12 23:35:00 -0600

    dropdown multiselect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,64 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Asegúrate de importar CommonModule
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SelectorsComponent } from './selectors/selectors.component';
-
+import { BmbTopBarComponent, 
+  BmbThemeComponent, 
+  BmbCardComponent,
+  BmbCardContentComponent, 
+  BmbCardFooterComponent, 
+  BmbCardHeaderComponent,
+  BmbLayoutDirective,
+  BmbLayoutItemDirective,
+  BmbDropdownComponent,
+} from '@ti-tecnologico-de-monterrey-oficial/ds-ng';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, SelectorsComponent],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    BmbTopBarComponent, 
+    BmbThemeComponent, 
+    BmbCardComponent, 
+    BmbCardFooterComponent, 
+    BmbCardHeaderComponent,
+    BmbCardContentComponent,
+    BmbLayoutDirective,
+    BmbLayoutItemDirective,
+    BmbDropdownComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-test';
+  
+  // Control de formulario
+  dropdownControl = new FormControl();
+  
+  // Opciones seleccionadas
+  selectedOptions: string[] = [];
+
+  // Maneja el cambio de selección
+  handleSelectionChange(selected: string | string[]) {
+    if (Array.isArray(selected)) {
+      this.selectedOptions = selected; // Asigna las selecciones
+    } else {
+      const index = this.selectedOptions.indexOf(selected);
+      if (index === -1) {
+        this.selectedOptions.push(selected); // Agrega si no está presente
+      } else {
+        this.selectedOptions.splice(index, 1); // Elimina si ya estaba
+      }
+    }
+    this.dropdownControl.setValue(this.selectedOptions); // Actualiza el control
+  }
+
+  // Eliminar una opción seleccionada
+  removeSelection(index: number) {
+    this.selectedOptions.splice(index, 1); // Elimina la selección
+    this.dropdownControl.setValue(this.selectedOptions); // Actualiza el control
+  }
 }
